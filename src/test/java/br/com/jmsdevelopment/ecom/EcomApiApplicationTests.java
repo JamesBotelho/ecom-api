4988829880ed85494eb71e010a12985698542cda
JamesBotelho <james_oliveira@live.com>
2021-05-17 20:12:32 -0300

    Inicialização do projeto da api do e-com

---
+++ src/test/java/br/com/jmsdevelopment/ecom/EcomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jmsdevelopment.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
