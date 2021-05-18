cd167a1c7aa48ba98c06442fa5de151c702e1f8a
JamesBotelho <james_oliveira@live.com>
2021-05-18 18:18:41 -0300

    Inclusão do teste de alteração de senha

--- src/test/java/br/com/jmsdevelopment/ecom/service/ClienteServiceImplMockTest.java
+++ src/test/java/br/com/jmsdevelopment/ecom/service/ClienteServiceImplMockTest.java
@@ -7,12 +7,15 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import br.com.jmsdevelopment.ecom.builder.ClienteBuilder;
 import br.com.jmsdevelopment.ecom.builder.ClienteDtoBuilder;
+import br.com.jmsdevelopment.ecom.dto.cliente.ClienteAlteraSenhaDto;
 import br.com.jmsdevelopment.ecom.dto.cliente.ClienteDto;
 import br.com.jmsdevelopment.ecom.mappers.ClienteMapper;
 import br.com.jmsdevelopment.ecom.model.Cliente;
@@ -25,6 +28,9 @@ class ClienteServiceImplMockTest {
 	
 	@Mock
 	private ClienteMapper clienteMapper;
+	
+	@Captor
+	public ArgumentCaptor<Cliente> clienteCaptor;
 	
 	private Cliente cliente;
 	private ClienteDto clienteDto;
@@ -64,5 +70,13 @@ class ClienteServiceImplMockTest {
 	@Test
 	public void deve_RetornarRunTimeException_QuandoPesquisaPorIdNaoCadastrado() {
 		assertThrows(RuntimeException.class, () -> clienteService.recuperarClientePorId(2L));
+	}
+	
+	@Test
+	public void deve_RetornarExceptionAoAlterarSenha_QuandoInformaSenhaAtualInCorreta() {
+		ClienteAlteraSenhaDto clienteAlteraSenhaDto = new ClienteAlteraSenhaDto();
+		clienteAlteraSenhaDto.setSenhaAntiga("12345678");
+		clienteAlteraSenhaDto.setSenhaNova("1234567890");
+		assertThrows(RuntimeException.class, () -> clienteService.alterarSenha(1L, clienteAlteraSenhaDto));
 	}
 }
