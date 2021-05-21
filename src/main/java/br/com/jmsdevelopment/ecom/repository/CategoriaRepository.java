9a6ac002f10e39a15430b489378f7d4ce6a28b5f
JamesBotelho <james_oliveira@live.com>
2021-05-21 19:27:19 -0300

    Correção no import do JpaRepository

--- src/main/java/br/com/jmsdevelopment/ecom/repository/CategoriaRepository.java
+++ src/main/java/br/com/jmsdevelopment/ecom/repository/CategoriaRepository.java
@@ -3,6 +3,8 @@ package br.com.jmsdevelopment.ecom.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.jmsdevelopment.ecom.model.Categoria;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
