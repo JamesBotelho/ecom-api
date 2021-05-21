db91ac977083fdd2ea490ca57b9f3a7ecc5ef17a
JamesBotelho <james_oliveira@live.com>
2021-05-21 19:26:57 -0300

    Adicionada entidade de categoria de produtos, e relacionamento com o a entidade de produtos

--- src/main/java/br/com/jmsdevelopment/ecom/model/Produto.java
+++ src/main/java/br/com/jmsdevelopment/ecom/model/Produto.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -32,4 +34,7 @@ public class Produto {
 	private BigDecimal preco;
 	@Column(name = "PRECO_PROMOCIONAL", nullable = false)
 	private BigDecimal precoPromocional;
+	@ManyToOne
+	@JoinColumn(name = "ID_CATEGORIA", nullable = false)
+	private Categoria categoria;
 }
