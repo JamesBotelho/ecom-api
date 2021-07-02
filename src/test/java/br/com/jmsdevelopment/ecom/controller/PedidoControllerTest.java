7c234df5454c3e0a26ba26827bace2b96db040c1
JamesBotelho <james_oliveira@live.com>
2021-07-01 21:25:41 -0300

    Adicionado testes da inserção de pedido através do controller

---
+++ src/test/java/br/com/jmsdevelopment/ecom/controller/PedidoControllerTest.java
@@ -0,0 +1,125 @@
+package br.com.jmsdevelopment.ecom.controller;
+
+import br.com.jmsdevelopment.ecom.builder.PedidoDtoBuilder;
+import br.com.jmsdevelopment.ecom.dto.pedido.PedidoDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class PedidoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private URI uri;
+
+    @BeforeEach
+    public void beforeEach() throws URISyntaxException {
+        uri = new URI("/pedido");
+    }
+
+    @Test
+    public void deve_retornarStatus400_QuandoInserePedidoSemItem() throws Exception {
+        PedidoDto pedidoDto = new PedidoDtoBuilder()
+                .comCliente(1L, null, null)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(mapToJson(pedidoDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+    @Test
+    public void deve_retornarStatus400_QuandoInserePedidoSemCliente() throws Exception {
+        PedidoDto pedidoDto = new PedidoDtoBuilder()
+                .comItemPedido(1L, null, 1, new BigDecimal(40))
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(mapToJson(pedidoDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+    @Test
+    public void deve_retornarStatus200eIdDiferenteDeZero_QuandoInserePedido() throws Exception {
+        PedidoDto pedidoDto = new PedidoDtoBuilder()
+                .comItemPedido(1L, null, 1, new BigDecimal(40))
+                .comCliente(1L, null, null)
+                .build();
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(mapToJson(pedidoDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        PedidoDto pedidoRetornado = mapFromJson(mvcResult.getResponse().getContentAsString(), PedidoDto.class);
+
+        assertNotNull(pedidoRetornado.getId());
+        assertNotEquals(0L, pedidoRetornado.getId());
+    }
+
+    @Test
+    public void deve_RetornarStatus400_QuandoValorDoProdutoEhInvalido() throws Exception {
+        PedidoDto pedidoDto = new PedidoDtoBuilder()
+                .comItemPedido(1L, null, 1, new BigDecimal(30))
+                .comCliente(1L, null, null)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(mapToJson(pedidoDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+    @Test
+    public void deve_RetornarStatus400_QuandoQuantidadeIgualaZero() throws Exception {
+        PedidoDto pedidoDto = new PedidoDtoBuilder()
+                .comItemPedido(1L, null, 0, new BigDecimal(40))
+                .comCliente(1L, null, null)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(mapToJson(pedidoDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+    private String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    private <T> T mapFromJson(String json, Class<T> clazz)
+            throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+}
